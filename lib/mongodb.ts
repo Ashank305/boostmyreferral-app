5ffba0f3e2eeebfd417541ed938741c79b523471
Ashank305 <ashankarora305@gmail.com>
2025-07-16 12:30:25 +0530

    Fix: typesafe mongodb connection and removed any usage

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,10 +6,21 @@ if (!MONGODB_URI) {
   throw new Error('❌ Please define MONGODB_URI in .env.local');
 }
 
-let cached = (global as any).mongoose;
+// Extend global type to allow custom `mongoose` property
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  } | undefined;
+}
+
+let cached = global.mongooseCache;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = global.mongooseCache = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export async function connectToDB() {
@@ -18,8 +29,6 @@ export async function connectToDB() {
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
       dbName: 'boosterDB',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
   }
 
